a3c31c1d59ad514f55f3a433c05643209b1023bb
Lionel Millaud <lionel.millaud2026@campus-eni.fr>
2026-07-30 10:28:42 +0200

    Ajout des filtres des sorties

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,19 +6,73 @@ use App\Repository\CampusRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 final class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(SortieRepository $sortieRepository, CampusRepository $campusRepository): Response
-    {
-        $campus = $this->getUser()->getCampus();
+    public function index(
+        Request $request,
+        SortieRepository $sortieRepository,
+        CampusRepository $campusRepository
 
-        $sorties = $sortieRepository->findSortiesParCampus($campus);
+    ): Response
+    {
+
 
         $campusList = $campusRepository->findAll();
+
+        $campusId = $request->query->get('campus');
+
+        $nomSortie = $request->query->get('nom');
+
+        $dateDebut = $request->query->get('dateDebut');
+
+        $dateFin = $request->query->get('dateFin');
+
+        $organisateur = $request->query->getBoolean('organisateur');
+
+        $inscrit = $request->query->getBoolean('inscrit');
+
+        $nonInscrit = $request->query->getBoolean('nonInscrit');
+
+        $passees = $request->query->getBoolean('passees');
+
+        $participant = $this->getUser();
+
+        if ($campusId){
+            $campus = $campusRepository->find($campusId);
+
+            $sorties = $sortieRepository->findSortiesParCampus(
+                $campus,
+                $nomSortie,
+                $dateDebut,
+                $dateFin,
+                $organisateur,
+                $inscrit,
+                $nonInscrit,
+                $passees,
+                $participant
+            );
+        } else {
+            $campus = $this->getUser()->getCampus();
+
+            $sorties = $sortieRepository->findSortiesParCampus(
+                $campus,
+                $nomSortie,
+                $dateDebut,
+                $dateFin,
+                $organisateur,
+                $inscrit,
+                $nonInscrit,
+                $passees,
+                $participant
+            );
+
+        }
+
 
         return $this->render('accueil/index.html.twig', [
             'sorties' => $sorties,
